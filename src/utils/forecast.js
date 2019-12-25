73a701f600203f9ae4cf61e0649d6c0ab1ca4d12
kiharayuta <k.kyuta.821@gmail.com>
2019-12-25 11:07:57 -0800

    Add celcius symbol on forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -14,7 +14,7 @@ const forecast = (latitude, longitude, callback) => {
         }
         else {
             const curr = body.currently;
-            callback(undefined, `${body.daily.data[0].summary} It is currently ${curr.temperature} degrees out and ${curr.precipIntensity}% chance of rain.`);
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${curr.temperature}°C out and ${curr.precipIntensity}% chance of rain.`);
         }
     });
 }
